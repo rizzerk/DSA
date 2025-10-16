e8b613858eb6a36cac2592b834e78d0f82a1dbb4
rizzerk <24101986@usc.edu.ph>
2025-10-16 13:10:25 +0800

    rename

---
+++ .history/SETS/Notes/assignment/excercise2_20251013185607.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 10
+
+typedef struct node{
+    int data;
+    struct node * next;
+}*SET;
+
+typedef SET Dictionary[MAX];
+
+void initDictionary(Dictionary A){
+   for(int i=0; i < MAX; i++){
+    A[i] = NULL;
+   }
+}
+
+int main(){
+    Dictionary A;
+
+    initDictionary(A);
+}
